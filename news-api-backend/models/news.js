857ad28f203e43e7dddef9513d31e1f697ca8d7f
Divij Chopra <97387962+dxxxj@users.noreply.github.com>
2024-08-03 13:21:27 +0530

    Update news.js

--- news-api-backend/models/news.js
+++ news-api-backend/models/news.js
@@ -1,13 +1,10 @@
-// Import the mongoose module
 const mongoose = require('mongoose');
 
-// Define the schema for news articles
 const NewsSchema = new mongoose.Schema({
-    title: String,        // Title of the news article
-    description: String,  // Description of the news article
-    url: String,          // URL to the full news article
-    publishedAt: Date,    // Date when the article was published
+    title: String,        
+    description: String,  
+    url: String,       
+    publishedAt: Date,   
 });
 
-// Create a model based on the schema
 module.exports = mongoose.model('News', NewsSchema);
